5f401ec6f75ecc07572f0fb63c082f6487e7fb31
Isaias <user.moralezisaias.1@gmail.com>
2025-02-17 22:02:21 -0300

    Trabajo practico numero 4

---
+++ repository/superheroesDataSource.mjs
@@ -0,0 +1,7 @@
+// repository/superheroesDataSource.mjs
+export default class SuperheroesDataSource {
+    //metodo abstracto para obtener todos los superheroes
+    obtenerTodos() {
+        throw new Error('Este método debe ser implementado por la subclase');
+    }
+}
---
+++ repository/superheroesRepository.mjs
@@ -0,0 +1,20 @@
+//repositori/superheroesRepository.mjs
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import SuperheroesDataSource  from './superheroesDataSource.mjs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default class SuperheroesFileRespository extends SuperheroesDataSource {
+    constructor() {
+        super();
+        this.filePath = path.join(__dirname, '../superheroes.txt');
+    }
+
+    obtenerTodos() {
+        const data = fs.readFileSync(this.filePath, 'utf-8');
+        return JSON.parse(data); //Convierte el archivo JSON en un array de objetos JS
+    }
+}
